963b99f814333e0eeba45ab492e91390fb1aa883
Aliaksei Paleshchuk <Aliaksei_Paleshchuk@epam.com>
2021-02-23 13:15:11 +0300

    Task 2: Webpack

---
+++ README.md
@@ -0,0 +1,23 @@
+# Definition
+
+The idea is to create a single page application, which will allow users to search, add and delete movies from the Movies DB database.
+
+
+## Installation
+
+Use the package manager npm to install project.
+
+```bash
+npm install
+```
+
+## Usage
+
+```bash
+npm run dev
+npm run build
+npm run serve
+```
+
+## License
+No License
---
+++ config/webpack.dev.js
@@ -0,0 +1,81 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const webpack = require('webpack');
+
+function buildConfig(configDirs) {
+    return {
+        entry: configDirs.APP_DIR + '/index.js',        
+        output: {
+            path: configDirs.BUILD_DIR,
+            filename: 'bundle.js',
+            publicPath: '/'
+        },
+        mode: 'development',
+        devServer: {
+            contentBase: configDirs.BUILD_DIR,
+            compress: false,
+            port: 3000,
+            historyApiFallback: true
+        },
+        resolve: {
+            alias: {
+                components: configDirs.APP_DIR + '/components/'
+            },
+            extensions: ['.js', '.jsx']
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    use: ["source-map-loader"],
+                    enforce: "pre"
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: 'babel-loader'
+                },
+                {
+                    test: /\.(png|svg|jpg|gif|ttf|eot|woff)$/,
+                    use: ["file-loader"]
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                importLoaders: 1,
+                                modules: true,
+                                localIdentName: '[local]___[hash:base64:5]'
+                            }
+                        }
+                    ],
+                    include: /\.module\.css$/
+                },
+                {
+                    test: /\.css$/,
+                    use: [
+                      'style-loader',
+                      'css-loader'
+                    ],
+                    exclude: /\.module\.css$/
+                }
+            ]
+        },
+        devtool: false,
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: configDirs.APP_DIR + '/index.html'
+            }),
+            new webpack.SourceMapDevToolPlugin({            
+                filename: 'index.js.map',
+                exclude: ['vendor.js']
+            }),
+            new MiniCssExtractPlugin()
+        ]
+    };
+};
+
+module.exports = buildConfig;
